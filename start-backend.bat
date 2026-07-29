0992c5537da6c13176869255fe4449ba52c98c34
Mohamed-Hammada <91836396+Mohamed-Hammada@users.noreply.github.com>
2026-07-29 20:36:29 +0300

    fast startup

--- start-backend.bat
+++ start-backend.bat
@@ -4,6 +4,20 @@ echo ========================================================
 echo Starting Bemo HR Spring Boot Backend (Port 8080)...
 echo ========================================================
 echo.
+
+set "JAVA_HOME=C:\Users\wolfn\scoop\apps\openjdk26\current"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+
 cd /d "%~dp0be"
-call .\gradlew.bat bootRun --args="--spring.profiles.active=dev"
+
+call gradlew bootJar
+
+java ^
+ -Xms512m ^
+ -Xmx2g ^
+ -jar build\libs\hr-platform-0.0.1-SNAPSHOT.jar ^
+ --spring.profiles.active=dev
+
+
+
 pause
