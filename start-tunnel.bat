5274d116805cd6cb7c4abb3d74b9ee6e8bdc5855
Mohamed-Hammada <91836396+Mohamed-Hammada@users.noreply.github.com>
2026-08-08 13:00:02 +0300

    Update start-tunnel.bat

--- start-tunnel.bat
+++ start-tunnel.bat
@@ -2,8 +2,8 @@
 title Bemo Cloudflare Tunnel Launcher
 echo ========================================================
 echo Starting Cloudflare Tunnel for Bemo Angular Frontend...
-echo Target: http://localhost:4200 (Host Header: localhost:4200)
+echo Target: http://localhost:8080 (Host Header: localhost:8080)
 echo ========================================================
 echo.
-"%~dp0cloudflared-windows-amd64.exe" tunnel --url http://localhost:4200 --http-host-header localhost:4200
+"%~dp0cloudflared-windows-amd64.exe" tunnel --url http://localhost:8080 --http-host-header localhost:8080
 pause
